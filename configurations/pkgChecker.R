331092189d2d025d12a8135c6172a4a0a5ec97f3
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-10-24 19:22:20 +0200

    Adds utils files and package loader script

---
+++ configurations/pkgChecker.R
@@ -0,0 +1,33 @@
+packages.list <- c("devtools", "ini","httr", "lubridate", "crayon", "tibble", "rlist")
+message("[PkgChecker][INFO] Package Manager")
+
+checkPackages <- function(packages){
+  new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+  if (length(new.packages)) {
+    message("[PkgChecker][checkPackages][INFO]",length(new.packages)," packages needed to execute aplication\n Installing packages...")
+    suppressMessages(install.packages(new.packages,repos = "https://ftp.cixug.es/CRAN/", verbose = FALSE))
+  }
+}
+
+loadPackages <- function(packages) {
+  unload.packages <- packages[!(packages %in% (.packages() ) )  ]
+  if (length(unload.packages) > 0) {
+    message("[PkgChecker][loadPackages][INFO] ",length(unload.packages)," required packages not loaded. Loading packages ...")
+    for (lib in unload.packages ) {
+      message("[PkgChecker][loadPackages][INFO] ",lib)
+      library(lib, verbose = FALSE, quietly = FALSE, character.only = TRUE)
+    }
+  } else message("[PkgChecker][loadPackages][INFO] All packages are loaded")
+}
+
+verifyandLoadPackages <- function() {
+  checkPackages(packages.list)
+  loadPackages(packages.list)
+}
+
+verifyandLoadPackages()
+
+rm(packages.list)
+rm(checkPackages)
+rm(loadPackages)
+rm(verifyandLoadPackages)
---
+++ util/write_csv.R
@@ -0,0 +1,12 @@
+write_csv = function(csv, output.path) {
+  write.table(x = csv,
+              file = output.path,
+              append = TRUE,
+              sep = ";",
+              dec = ".",
+              quote = TRUE,
+              col.names = !file.exists(output.path),
+              row.names = FALSE,
+              qmethod = c("double"),
+              fileEncoding = "UTF-8")
+}
