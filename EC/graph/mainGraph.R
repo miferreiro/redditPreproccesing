78fce05f186a55f7540629ad2b2ddb68dfd32739
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-12-29 01:35:59 +0100

    First version of script to create a graph of the data

---
+++ EC/graph/mainGraph.R
@@ -0,0 +1,26 @@
+source("src/pkgChecker.R")
+library(igraph)
+message(green("[Classification][INFO] Starting classification..."))
+#https://kateto.net/networks-r-igraph
+#https://www.jessesadler.com/post/network-analysis-with-r/
+dtm <- readRDS("../preprocessing/files_output_tm/dtm_final_1000.rds")
+matrix <- as.matrix(dtm)
+# matrix[matrix >= 1] <- 1
+termMatrix <- t(matrix) %*% matrix
+saveRDS(termMatrix, "termMatrix.rds")
+g <- igraph::graph.adjacency(termMatrix, weighted = T, mode = "lower")
+plot(g, edge.color="orange", vertex.color="gray50")
+rglplot(g)
+
+
+
+deg.dist <- degree_distribution(g, cumulative=T, mode="all")
+
+plot( x=0:max(deg), y=1-deg.dist, pch=19, cex=1.2, col="orange",
+
+      xlab="Degree", ylab="Cumulative Frequency")
+
+
+kc <- coreness(g, mode="all")
+colrs <- adjustcolor( c("gray50", "tomato", "gold", "yellowgreen"), alpha=.6)
+plot(g, vertex.size=kc*6, vertex.label=kc, vertex.color=colrs[kc])
