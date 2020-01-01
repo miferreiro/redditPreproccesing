3ac5e801c09ea10446fb26af4692c23e678a326f
miferreiro <miguel.ferreiro.diaz@gmail.com>
2020-01-02 00:13:06 +0100

    Refactor scripts to representation data through graphs

--- EC/graph/mainGraph.R
+++ EC/graph/mainGraph.R
@@ -1,26 +1,40 @@
 source("src/pkgChecker.R")
-library(igraph)
-message(green("[Classification][INFO] Starting classification..."))
 #https://kateto.net/networks-r-igraph
 #https://www.jessesadler.com/post/network-analysis-with-r/
-dtm <- readRDS("files_input/out_final_dtm_tm.rds")
+#################################################################
+##                            GRAPH                            ##
+#################################################################
+
+message(green("[Graph][INFO] Starting creating graph using igraph package..."))
+
+dtm <- readRDS("files_input/out_dtm_IG.rds")
+# dtm <- readRDS("files_input/out_dtm_tfidf_IG.rds")
+
 matrix <- as.matrix(dtm)
 # matrix[matrix >= 1] <- 1
 termMatrix <- t(matrix) %*% matrix
+
 saveRDS(termMatrix, "files_output/termMatrix.rds")
-g <- igraph::graph.adjacency(termMatrix, weighted = T, mode = "lower")
-plot(g, edge.color="orange", vertex.color="gray50")
+termMatrix <- readRDS("files_output/termMatrix.rds")
+
+g <- igraph::graph.adjacency(termMatrix, weighted = T, mode = "directed")
+
+# plot(g, edge.color = "orange", vertex.color = "gray50")
+plot(g, layout = layout_with_graphopt, edge.arrow.size = 0.2)
+
 rglplot(g)
 
 
 
-deg.dist <- degree_distribution(g, cumulative=T, mode="all")
+deg.dist <- degree_distribution(g, cumulative = T, mode = "all")
 
-plot( x=0:max(deg), y=1-deg.dist, pch=19, cex=1.2, col="orange",
+plot( x = 0:max(deg), y = 1-deg.dist, pch = 19, cex = 1.2, col = "orange",
 
-      xlab="Degree", ylab="Cumulative Frequency")
+      xlab = "Degree", ylab = "Cumulative Frequency")
 
 
-kc <- coreness(g, mode="all")
-colrs <- adjustcolor( c("gray50", "tomato", "gold", "yellowgreen"), alpha=.6)
-plot(g, vertex.size=kc*6, vertex.label=kc, vertex.color=colrs[kc])
+kc <- coreness(g, mode = "all")
+colrs <- adjustcolor( c("gray50", "tomato", "gold", "yellowgreen"), alpha = .6)
+plot(g, vertex.size = kc*6, vertex.label = kc, vertex.color = colrs[kc])
+
+message(green("[Graph][INFO] Finish creating graph using igraph package!"))
---
+++ EC/graph/src/read_csv.R
@@ -0,0 +1,10 @@
+read_csv = function(csv) {
+  message(blue("[Graph][INFO] Reading csv from", csv))
+  read.csv(file = csv,
+           header = TRUE,
+           sep = ";",
+           dec = ".",
+           fill = FALSE,
+           stringsAsFactors = FALSE,
+           encoding = "UTF-8")
+}
