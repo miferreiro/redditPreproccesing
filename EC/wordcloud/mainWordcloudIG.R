5ed2ea00eb5bfaf76d1d20db884ee6442ed9b79d
miferreiro <miguel.ferreiro.diaz@gmail.com>
2020-01-01 23:49:04 +0100

    Fix the selection of terms to plot on the wordcloud

--- EC/wordcloud/mainWordcloudIG.R
+++ EC/wordcloud/mainWordcloudIG.R
@@ -7,17 +7,19 @@ source("src/read_csv.R")
 
 message(green("[Wordcloud][INFO] Starting plotting wordclouds using wordcloud package..."))
 
-# csv <- read_csv(csv = "files_input/outPre_all_comments_september.csv")
-# final.ig <- readRDS("files_input/out_IG.rds")
-final.ig <- readRDS("files_input/out_tfidf_IG.rds")
+final.ig <- readRDS("files_input/out_IG.rds")
+# final.ig <- readRDS("files_input/out_tfidf_IG.rds")
 
 final.ig.n <- as.numeric(final.ig)
 names(final.ig.n) <- names(final.ig)
 final.ig <- final.ig.n
-# pdf("files_output/IG_wordcloud.pdf")
-pdf("files_output/tfidf_IG_wordcloud.pdf")
-plot <- wordcloud::wordcloud(names(final.ig),
-                             final.ig,
+
+final.ig <- sort(final.ig, decreasing = T)
+
+pdf("files_output/IG_wordcloud.pdf")
+# pdf("files_output/tfidf_IG_wordcloud.pdf")
+plot <- wordcloud::wordcloud(names(final.ig)[1:50],
+                             final.ig[1:50],
                              max.words = 50,
                              scale = c(4, .2),
                              colors = RColorBrewer::brewer.pal(8, "Dark2"))
