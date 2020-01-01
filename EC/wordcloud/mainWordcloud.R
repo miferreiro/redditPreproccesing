a43fc6249952e2b12469243c554743c960987e7f
miferreiro <miguel.ferreiro.diaz@gmail.com>
2020-01-01 23:48:00 +0100

    Improves script to support large DocumentTermMatrix

--- EC/wordcloud/mainWordcloud.R
+++ EC/wordcloud/mainWordcloud.R
@@ -8,18 +8,25 @@ source("src/read_csv.R")
 message(green("[Wordcloud][INFO] Starting plotting wordclouds using wordcloud package..."))
 
 csv <- read_csv(csv = "files_input/outPre_all_comments_september.csv")
-dtm <- readRDS("files_input/out_final_dtm_tm.rds")
-# dtm <- readRDS("files_input/out_final_dtm_tfidf_tm.rds")
+target <- csv$subreddit
+rm(csv)
 
-for (subreddit in unique(csv$subreddit)) {
+
+for (subreddit in unique(target)) {
   message(blue("[Wordcloud][INFO] Plotting", subreddit, "wordcloud"))
 
-  freq <- colSums(as.matrix(dtm[csv$subreddit == subreddit, ]))
+  # dtm <- readRDS("files_input/out_final_dtm_tm.rds")
+  dtm <- readRDS("files_input/out_final_dtm_tfidf_tm.rds")
+
+  d <- dtm[target == subreddit, ]
+  rm(dtm)
+  m <- as.matrix(d[,findFreqTerms(d, 10)])
+  freq <- colSums(m)
   freq <- sort(freq,
                decreasing = TRUE)
 
-  pdf(paste0("files_output/", subreddit, "_dtm_tm_wordcloud.pdf"))
-  # pdf(paste0("files_output/", subreddit, "_dtm_tfidf_tm_wordcloud.pdf"))
+  # pdf(paste0("files_output/", subreddit, "_dtm_tm_wordcloud.pdf"))
+  pdf(paste0("files_output/", subreddit, "_dtm_tfidf_tm_wordcloud.pdf"))
   plot <- wordcloud::wordcloud(names(freq),
                                freq,
                                max.words = 50,
@@ -27,6 +34,7 @@ for (subreddit in unique(csv$subreddit)) {
                                colors = RColorBrewer::brewer.pal(8, "Dark2"))
   dev.off()
   rm(freq)
+  rm(plot)
 }
 
 message(green("[Wordcloud][INFO] Finish plotting wordclouds using wordcloud package..."))
