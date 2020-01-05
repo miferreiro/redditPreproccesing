096b34c53debc4b0fe62eec962ff9bdc02d34ca9
miferreiro <miguel.ferreiro.diaz@gmail.com>
2020-01-05 05:38:02 +0100

    Adds the creation of different clusters using hierarchical clustering
    + Also, it included the clusters created stats.

--- EC/clustering/hierarchalClustering.R
+++ EC/clustering/hierarchalClustering.R
@@ -1,37 +1,59 @@
 source("src/pkgChecker.R")
 
+diste <- readRDS("files_output/out_dist_euclidian.rds")
+distet <- readRDS("files_output/out_tfidf_dist_euclidian.rds")
+
+distp <- readRDS("files_output/out_dist_pearson.rds")
+distpt <- readRDS("files_output/out_tfidf_dist_pearson.rds")
+
 #################################################################
-##                    Hierarchal Clustering                    ##
+##                  Hierarchical Clustering                    ##
 #################################################################
 
-dtm <- readRDS("files_input/out_dtm_IG.rds")
-# dtm <- readRDS("files_input/out_dtm_tfidf_IG.rds")
 
-dist <- readRDS("files_output/out_dist_euclidian.rds")
-# dist <- readRDS("files_output/out_tfidf_dist_euclidian.rds")
-
-set.seed(123)
-fit <- hclust(d = dist, method = "ward.D2")
-factoextra::fviz_dend(fit, cex = 0.5, k = 4, palette = "jco")
-
-pheatmap::pheatmap(t(dtm), cutree_cols = 4)
+# set.seed(123)
+# fit <- hclust(d = dist, method = "average")
+# cor(dist, cophenetic(fit))
+# factoextra::fviz_dend(fit, cex = 0.2, k = 10, color_labels_by_k = TRUE,  palette = "jco")
+#
 
 
-# plot(fit, hang = -1)
-#
-# groups <- cutree(fit, k = 10)
-# rect.hclust(fit, k = 10, border = "red")
-#
-#
-#
-# fit <- pvclust(dtm, method.hclust="ward",
-#                method.dist="euclidean")
-# plot(fit)
-# pvrect(fit, alpha=.95)
-#
-# # Model Based Clustering
-# library(mclust)
-# fit <- Mclust(dtm)
-# saveRDS(fit, "files_output/out_model_based_clustering_mclust.rds")
-# plot(fit) # plot results
-# summary(fit) # display the best model
+cfitEu <- hcut(x = diste, k = 4, hc_method = "ward.D", hc_metric = "euclidean")
+pdf(paste0("files_output/out_dist_euclidian_hierarClus.pdf"))
+factoextra::fviz_dend(cfitEu, k = 4, rect = TRUE, cex = 0.5,  pallete = "jco")
+dev.off()
+
+cfitEuTf <- hcut(x = distet, k = 4, hc_method = "ward.D", hc_metric = "euclidean")
+pdf(paste0("files_output/out_tfidf_dist_euclidian_hierarClus.pdf"))
+factoextra::fviz_dend(cfitEuTf, k = 4, rect = TRUE, cex = 0.5,  pallete = "jco")
+dev.off()
+
+cfitPe <- hcut(x = distp, k = 4, hc_method = "ward.D", hc_metric = "pearson")
+pdf(paste0("files_output/out_dist_pearson_hierarClus.pdf"))
+factoextra::fviz_dend(cfitPe, k = 4, rect = TRUE, cex = 0.5, pallete = "jco")
+dev.off()
+
+cfitPeTf <- hcut(x = distpt, k = 4, hc_method = "ward.D", hc_metric = "pearson")
+pdf(paste0("files_output/out_tfidf_dist_pearson_hierarClus.pdf"))
+factoextra::fviz_dend(cfitPeTf, k = 4, rect = TRUE, cex = 0.5, pallete = "jco")
+dev.off()
+
+table(cfitEu$cluster)
+table(cfitEuTf$cluster)
+table(cfitPe$cluster)
+table(cfitPeTf$cluster)
+
+scfitEu <- fpc::cluster.stats(d = diste, cfitEu$cluster)
+scfitEuTf <- fpc::cluster.stats(d = distet, cfitEuTf$cluster)
+scfitPe <- fpc::cluster.stats(d = distp, cfitPe$cluster)
+scfitPeTf <- fpc::cluster.stats(d = distpt, cfitPeTf$cluster)
+
+scfitEu$dunn
+scfitEu$clus.avg.silwidths
+scfitEuTf$dunn
+scfitEuTf$clus.avg.silwidths
+scfitPe$dunn
+scfitPe$clus.avg.silwidths
+scfitPeTf$dunn
+scfitPeTf$clus.avg.silwidths
+
